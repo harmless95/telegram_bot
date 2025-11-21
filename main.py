dab7c5f2b5eb339e740a5c5e87d57ac73cb0eccd
Vitalya <git@github.com:harmless95/RabbitMQ.git>
2025-11-21 12:45:34 +0700

    correct

--- main.py
+++ main.py
@@ -6,23 +6,22 @@ from aiogram.enums.parse_mode import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 
-import config
+from core import config
 from api.handlers import router
 
-loger = logging.getLogger(__name__)
 
 async def main() -> None:
-    loger.warning("11")
-    bot = Bot(token=config.BOT_TOKEN,
-              default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    loger.warning("22")
+
+    bot = Bot(
+        token=config.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router=router)
-    loger.warning("33")
-    # await bot.delete_webhook(drop_pending_updates=True)
-    # loger.warning("44")
+
+    await bot.delete_webhook(drop_pending_updates=True)
+
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
-    loger.warning("55")
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
