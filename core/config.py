828f98b96208cc192ed698ec8bab6513a354003c
Vitalya <git@github.com:harmless95/RabbitMQ.git>
2025-11-22 17:07:08 +0700

    create Base.py DB postgresql. Corrected the names of tables and columns

--- core/config.py
+++ core/config.py
@@ -1,13 +1,29 @@
 from pathlib import Path
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 class TelegramBot(BaseModel):
     token: str
+
+
+class PostgresDB(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 class Setting(BaseSettings):
@@ -21,6 +37,7 @@ class Setting(BaseSettings):
         env_prefix="APP_CONFIG__",
     )
     t_bot: TelegramBot
+    db: PostgresDB
 
 
 setting = Setting()
