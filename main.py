b2ac198bbe412f99d6af69b78e4b5122f3af67b3
Vitalya <git@github.com:harmless95/RabbitMQ.git>
2025-11-22 17:10:35 +0700

    We wrapped the application so that it would automatically close the connection.

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 import logging
 import asyncio
+from contextlib import asynccontextmanager
 
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 
+from core.model.helper_db import db_helper_conn
 from core.config import setting
 from api.handlers import router
 
 
+@asynccontextmanager
+async def lifespan():
+    try:
+        yield
+    finally:
+        await db_helper_conn.dispose()
+
+
 async def main() -> None:
+    async with lifespan(None):
+        bot = Bot(
+            token=setting.t_bot.token,
+            default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+        )
+        dp = Dispatcher(storage=MemoryStorage())
+        dp.include_router(router=router)
 
-    bot = Bot(
-        token=setting.t_bot.token,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
-    )
-    dp = Dispatcher(storage=MemoryStorage())
-    dp.include_router(router=router)
+        await bot.delete_webhook(drop_pending_updates=True)
 
-    await bot.delete_webhook(drop_pending_updates=True)
-
-    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+        await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 
 if __name__ == "__main__":
